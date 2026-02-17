fa4992ace3b119d6fcad2afa75276f98c466318f
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2026-02-17 19:24:15 -0300

    sway + new wallpapers

--- .config/hypr/scripts/theme_switcher.sh
+++ .config/hypr/scripts/theme_switcher.sh
@@ -5,7 +5,7 @@ WALLPAPER_DIR="$HOME/.config/backgrounds"
 
 # 1. Select wallpaper using Rofi
 # We list files in the dir, pipe to rofi, and save selected file to variable
-SELECTED=$(ls "$WALLPAPER_DIR" | rofi -dmenu -p "Select Theme/Wallpaper")
+SELECTED=$(ls "$WALLPAPER_DIR" | wofi -dmenu -p "Select Theme/Wallpaper")
 
 # Exit if no selection (user pressed Esc)
 if [ -z "$SELECTED" ]; then
@@ -15,7 +15,7 @@ fi
 FULL_PATH="$WALLPAPER_DIR/$SELECTED"
 
 # 2. Ask for Light or Dark Mode
-MODE=$(echo -e "Dark\nLight" | rofi -dmenu -p "Select Mode")
+MODE=$(echo -e "Dark\nLight" | wofi -dmenu -p "Select Mode")
 
 # 3. Generate Colors
 # We use 'haishoku' backend for better contrast
@@ -29,19 +29,21 @@ fi
 # 4. Apply Wallpaper (Wait for generation to finish first)
 swww img "$FULL_PATH" --transition-type grow --transition-pos 0.9,0.9 --transition-step 90
 
+killall swaync
+swaync &
+
 # 5. Reload Apps
 # Reload Waybar
 killall -SIGUSR2 waybar
+
+# Send escape sequences to all open terminals to update colors immediately
+# (This fixes the background/foreground without needing a full reload)
+wal -R
+
+killall -SIGUSR1 kitty
 
 # Reload Hyprland
 hyprctl reload
 
 # Update Pywalfox (Zen)
 pywalfox update
-
-# Update Discord (Vencord)
-# (Ensure you have linked the pywal css as mentioned in the previous step)
-pywal-discord -t default
-
-# Send Notification
-notify-send "Theme Set" "Applied $MODE theme from $SELECTED"
